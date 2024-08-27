a047e0df185db36a008a0d4ecee2e921877eac0c
Gabriel Trifoni <gabrielsoavetrifoni12@gmail.com>
2024-08-27 12:30:47 -0300

    updatephoto.dto

--- src/modules/photo/photos.controller.ts
+++ src/modules/photo/photos.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { Photo } from "../../entities/photo.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreatePhotoDto } from "./dto/create-photo.dto";
+import { UpdatePhotoDto } from "./dto/update-photo.dto";
 
 @Controller('/photos')
 export class PhotosController {
@@ -13,5 +15,34 @@ export class PhotosController {
     @Get()
     async findAll() {
         return await this.repository.find();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id', ParseIntPipe) id: number) {
+      return await this.repository.findOneBy({ id });
+    }
+  
+    @Post()
+    async create(@Body() input: CreatePhotoDto) {
+      return await this.repository.save({
+        ...input,
+      });
+    }
+  
+    @Patch(':id')
+    async update(@Param('id') id, @Body() input: UpdatePhotoDto) {
+      const cat = await this.repository.findOneBy({ id });
+  
+      return await this.repository.save({
+        ...cat,
+        ...input,
+      });
+    }
+  
+    @Delete(':id')
+    @HttpCode(204)
+    async delete(@Param('id') id: number) {
+      const cat = await this.repository.findOneBy({ id });
+      this.repository.delete(cat);
     }
 }
